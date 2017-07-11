01c2015eef3ea7311a727bb18c83c6194bf94b9f
Warren Panayides <wpanayides@gmail.com>
2017-07-11 17:10:15 +0200

    Built app 1
    Built app 1

--- Class 1 - Hello World/Class1.py
+++ Class 1 - Hello World/Class1.py
@@ -1,1 +1,9 @@
-test1 
+print("=============================================")
+print("                 HELLO APP")
+print("=============================================")
+print()
+usertext = input("What is your name? ")
+greeting = "Hello "
+greeting2 = " nice to meet you!"
+print(greeting + usertext + greeting2)
+input()
