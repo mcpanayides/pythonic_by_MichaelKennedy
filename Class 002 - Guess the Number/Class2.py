897eb87813a5eec8aa19052aa4c2d3dfb797ce3e
Warren Panayides <wpanayides@gmail.com>
2017-07-12 15:50:10 +0200

    Finished Program and Unified Code
    Finished Program and Unified Code

--- Class 002 - Guess the Number/Class2.py
+++ Class 002 - Guess the Number/Class2.py
@@ -1,20 +1,26 @@
 import random
 
-print("=============================================")
-print("            Guess that number game")
-print("=============================================")
+print('=============================================')
+print('            Guess that number game')
+print('=============================================')
 print()
 
-guess = "-100"
+guess = -100
 the_number = random.randint(0, 100)
-errortrap = ("I don't know what you mean, please try again.")
+
+name = input('Player what is your name? ')
 
 while guess != the_number:
-    guess_text = input("Guess a number between 0 and 100: ")
+    guess_text = input('Guess a number between 0 and 100: ')
     guess = int(guess_text)
+
     if guess < the_number:
-        print('Too Low')
+        print('Sorry {}, your guess of {} was too LOW'.format(name, guess))
     elif guess > the_number:
-        print('Too High')
-    else
-        print('errortrap')
+        print('Sorry {}, your guess of {} was too HIGH'.format(name, guess))
+    else:
+        print('You Win!')
+print('')
+print('')
+
+input('Press any key to Exit')
